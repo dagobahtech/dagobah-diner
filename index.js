1985d52bb3eafa2a98f4077ce00ba0d895296189
Daniele Lisi <danielelisi978@gmail.com>
2017-05-06 16:58:29 -0700

    Server side API to insert item into DB menu table

--- index.js
+++ index.js
@@ -33,13 +33,29 @@ app.use(session({
 app.use(express.static(path.join(__dirname, "client","/build")));
 
 app.get("/admin", function(req, resp) {
-    resp.sendFile(adminFolder + "admin.html");
+    resp.sendFile(adminFolder + "/admin.html");
 });
 
 app.post("/admin/createItem", function(req, resp) {
 
     console.log(req.body);
 
+    pg.connect(dbURL, function(err, client, done) {
+        if (err) {
+            console.log(err);
+        }
+
+        var dbQuery = "INSERT INTO menu (name, category, description, price, cook_time, kitchen_station_id) VALUES ($1, $2, $3, $4, $5, $6)";
+        client.query(dbQuery, [req.body.name, req.body.category, req.body.desc, req.body.price, req.body.time, req.body.station], function(err, result) {
+            if (err) {
+                console.log(err);
+                resp.end("ERROR");
+            }
+
+            resp.send({status: "success", msg: "item created!"})
+
+        });
+    });
 });
 
 io.on("connection", function(socket){
