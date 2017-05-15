6f0420978b5b0254685aa6fa450f4d38f098fac2
Jed Iquin <jedikin@gmail.com>
2017-05-15 13:44:39 -0700

    fixed bug, when discarding something in order that food tray has does not get that item

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ io.on("connection", function(socket){
         if(socket.channel === "kitchen") {
             kitchen.discard(fromOrder, itemIndex, orderIndex);
             if(fromOrder) {
-                io.to(socket.channel).emit("update", "order", kitchen._orderQueue.orders);
+                io.to(socket.channel).emit("orders", kitchen._orderQueue.orders, kitchen._foodTray.items);
             } else
             {
                 io.to(socket.channel).emit("update","foodtray", kitchen._foodTray.items);
