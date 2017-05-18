b2f7bfb979c94dfcbedabfffd71843ed8558e53f
Tommy Do <tommyd.o@outlook.com>
2017-05-18 14:08:33 -0700

    new functional, add menu items

--- tests/addManyBeverage.js
+++ tests/addManyBeverage.js
@@ -1,9 +1,22 @@
 module.exports = {
 
 	'Add 3 drinks': function(browser){
-		borwser
+		browser
 			.url('localhost:3000/order')
-			.waitForElementVisable('body',1000)
-			
+			.pause(2000)
+			.click("#menuButton3")
+			.pause(1000)
+			.click('#addButton-16')
+			.pause(200)
+			.click('#addButton-16')
+			.pause(200)
+			.click('#addButton-8')
+			.pause(200)
+			.click('#confirmButton')
+			.pause(1000)
+			.click('#yesConfirmButton')
+			.pause(2000)
+			.end()
+
 	}
 };
--- tests/addOneItem.js
+++ tests/addOneItem.js
@@ -2,12 +2,11 @@ module.exports = {
     'Add 1 order': function (browser) {
         browser
             .url('localhost:3000/order')
-            .waitForElementVisible('body', 1000)
+            .pause(2000)
             .click('#addButton-2')
             .click('#confirmButton')
             .click('#yesConfirmButton')
             .pause(2000)
-            .expect.elementIdElement('#orderConfirmation').to.be.an('h1')
             .end()
     }
 };
