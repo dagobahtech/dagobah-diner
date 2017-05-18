5e4b0f03f4ba47feff6e51b6346a1bc75ebd6ff6
Daniele Lisi <danielelisi978@gmail.com>
2017-05-18 16:37:27 -0700

    Revert "updated functional testing files"

--- tests/addMax.js
+++ tests/addMax.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'Add maximum' : function(browser){
 		browser
-			.url('http://localhost:3000/order')
+			.url('localhost:3000/order')
 			.pause(2000)
 			.click('#menuButton1')
 			.pause(200)
