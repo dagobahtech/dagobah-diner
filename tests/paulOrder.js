fe1fa0509c954a3e3f936d23d9a6f4c83d0a2881
Tommy Do <tommyd.o@outlook.com>
2017-05-18 17:44:56 -0700

    fixed http and made another test for max items

--- tests/paulOrder.js
+++ tests/paulOrder.js
@@ -5,16 +5,16 @@ module.exports = {
 
     'Paul Order': function(browser){
         browser
-            .url('localhost:3000/order')
-            .pause(2000)
+            .url('http://localhost:3000/order')
+            .pause(200)
             .click("#menuButton1")
-            .pause(1000)
+            .pause(100)
             .click('#addButton-3')
             .pause(200)
             .click('#addButton-3')
             .pause(200)
             .click('#confirmButton')
-            .pause(1000)
+            .pause(100)
             .click('#yesConfirmButton')
             .pause(2000)
             .assert.containsText('#orderConfirmation', 'Thank you for ordering')
