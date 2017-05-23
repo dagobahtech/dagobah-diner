b0e1011afc438de04cec3757347f6849e45a87c7
Tommy Do <tommyd.o@outlook.com>
2017-05-22 18:46:07 -0700

    ajax calls

---
+++ client/js/modify.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	var oldName = document.getElementById("");
+	var serialID = document.getElementById("");
+	var newName = document.getElementById("");
+	var newDescription = document.getElementById("");
+	var newCategory = document.getElementById("");
+	var newPrice = docuemnt.getElementById("");
+	var newCookTime = document.getElementById("");
+	var newStation = document.getElementById("");
+	var select = document.getElementById("");
+	var submit = document.getElementById("");
+
+	submit.addEventListener("click", function(){
+
+	});
+});
