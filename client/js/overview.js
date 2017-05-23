dd607b840dc5d300dcc0d5cd2eff76e3246e6ca8
Daniele Lisi <danielelisi978@gmail.com>
2017-05-23 13:09:32 -0700

    fixed font imports

--- client/js/overview.js
+++ client/js/overview.js
@@ -8,28 +8,11 @@ $(document).ready(function() {
 
 	socket.emit("load orders");
 
-	socket.on("test1", function(test1){
-		socket.emit("test2");
-	});
-
 	socket.on("orders", function(orders, ready){
 		console.log(orders);
 		console.log(ready);
-		document.getElementById("in-Progress").innerHTML = "";
-		document.getElementById("serving-Now").innerHTML = "";
-		for (var i = 0; i < orders.length; i++) {
-			var newOrder = document.createElement("div");
-			newOrder.innerHTML = orders[i]._orderNumber;
-			console.log(orders[i]._orderNumber);
-			newOrder.className = "order-Number col-11 offset-1 notServed";
-			document.getElementById("in-Progress").appendChild(newOrder);
-		}
-		for (var x = 0; x < ready.length; x++){
-			var nowServe = document.createElement("div");
-			nowServe.innerHTML = ready[x]._orderNumber;
-			nowServe.className = "order-Number col-11 offset-1 justServed";
-			document.getElementById("serving-Now").appendChild(nowServe);
-		}
+
+
 	});
 		
 });
